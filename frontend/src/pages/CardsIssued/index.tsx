a0f59470421bf8b39ded53bb38b1b1ddcca5e3f1
Heloisa Arfelli <Heloisa@uptech.local>
2024-04-05 16:54:25 -0300

    In Process

--- frontend/src/pages/CardsIssued/index.tsx
+++ frontend/src/pages/CardsIssued/index.tsx
@@ -4,19 +4,63 @@ import DownloadFacilitators from "../../components/layout/DownloadFacilitators";
 import Input from "../../components/shared/Input";
 import Select from "../../components/shared/Select";
 
-
 const PageCardsIssued: React.FC = () => {
+  return (
+    <>
+      <DefaultHeader sessionTheme="Cartões emitidos" />
 
-    return (
-        <>
-            <DefaultHeader sessionTheme="Cartões emitidos" />
+      <div className="container-production-report">
+        <Select info={"Selecione um tipo:"} name="activeType" onChange>
+          <option selected>Selecione um ativo...</option>
+          <option value="Sim">Dm Card</option>
+          <option value="Não">Rede Use</option>
+        </Select>
 
-          
+        <div className="container-inputs">
+          <div className="inputs">
+            <Input
+              name="fileName"
+              placeholder="Arquivo..."
+              info="Arquivo:"
+             
+            />
+          </div>
 
-            
-        </>
-    )
-}
+          <div className="inputs">
+            <Input
+              type="date"
+              name="InitialProcessingDate"
+              info="Data de processamento inicial:"
+             
+            />
 
+            <Input
+              type="date"
+              name="FinalProcessingDate"
+              info="Data de processamento final:"
+             
+            />
+          </div>
+
+          <div className="inputs">
+            <Input
+              type="date"
+              name="InitialShippingDate"
+              info="Data de expedição inicial:"
+              
+            />
+
+            <Input
+              type="date"
+              name="FinalShippingDate"
+              info="Data de expedição final:"
+             
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default PageCardsIssued;

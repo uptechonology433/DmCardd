6237e7a05ca9d55ab6265789ded937f09d6f17c6
Rebecaao <rebecasunaika@gmail.com>
2024-04-22 14:25:17 -0300

    grafico

--- backend/src/routes/index.php
+++ backend/src/routes/index.php
@@ -11,28 +11,30 @@ use App\Controllers\CardsIssuedReport\CardsIssuedReportController;
 use App\Controllers\Dispatched\DispatchedController;
 use App\Controllers\Graph\GraphController;
 use App\Controllers\Inactive\InactiveProductsController;
+use App\Controllers\MonthReport\MonthReportController;
 use App\Controllers\ProductionReport\ProductionReportController;
 
 use App\Controllers\Stock\StockController;
 use App\Controllers\Waste\WasteProductsController;
 use App\Controllers\Ruptures\RupturesProductsController;
 use App\DAO\VeroCard\CardsIssuedReport\CardsIssuedReportDAO;
+use App\DAO\VeroCard\MonthReport\MonthReportDAO;
 use App\Middlewares\adminConference;
 use App\Middlewares\jwtDateTime;
 
 
-$app = new \Slim\App(slimConfiguration()); 
+$app = new \Slim\App(slimConfiguration());
 
 // ================= Login ========================
 
-$app -> post('/login' , AuthController::class . ':login');
+$app->post('/login', AuthController::class . ':login');
 
-$app -> post('/refresh-token' , AuthController::class . ':refreshToken');
+$app->post('/refresh-token', AuthController::class . ':refreshToken');
 
-$app -> get('/decodfy' , function($request , $response) {
-    $response = $response->withJson($request -> getAttribute('jwt'));
-}) -> add(new jwtDateTime())
--> add(jwtAuth());
+$app->get('/decodfy', function ($request, $response) {
+    $response = $response->withJson($request->getAttribute('jwt'));
+})->add(new jwtDateTime())
+    ->add(jwtAuth());
 
 // ================================================
 
@@ -40,34 +42,34 @@ $app -> get('/decodfy' , function($request , $response) {
 // ================== Production ==================
 
 
-$app -> post('/production' , ProductionController::class . ':Productsinproduction')
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->post('/production', ProductionController::class . ':Productsinproduction')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
 // =================================================
 
 
 // ================== Awaiting release =============
 
-$app -> get('/awaiting-release' , AwaitingReleaseController::class . ':AwaitingRelease')
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->get('/awaiting-release', AwaitingReleaseController::class . ':AwaitingRelease')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
 // ==================================================
 
 
 // ================== Awaiting shipment =============
 
-$app -> get('/awaiting-shipment' , AwaitingShipmentController::class . ':AwaitingShipment')
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->get('/awaiting-shipment', AwaitingShipmentController::class . ':AwaitingShipment')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 // ==================================================
 
 // ================== Awaiting dispached =============
 
-$app -> get('/dispatched' , DispatchedController::class . ':Dispatched')
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->get('/dispatched', DispatchedController::class . ':Dispatched')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 // ==================================================
 
 
@@ -84,6 +86,14 @@ $app->post('/waste-products', WasteProductsController::class . ':getWasteProduct
     ->add(jwtAuth());
 // ==================================================
 
+$app->post('/graph', GraphController::class . ':getGraph')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
+
+$app->post('/monthReport', MonthReportController::class . ':getMontReport')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
+
 $app->post('/ruptures-products', RupturesProductsController::class . ':getRupturesProducts')
     ->add(new jwtDateTime())
     ->add(jwtAuth());
@@ -93,53 +103,53 @@ $app->post('/ruptures-products', RupturesProductsController::class . ':getRuptur
 
 // ================== Production Report =============
 
-$app -> post('/production-report' , ProductionReportController::class . ':ProductionReport')
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->post('/production-report', ProductionReportController::class . ':ProductionReport')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
-$app -> post('/cardsissued-report' , CardsIssuedReportController::class . ':CardsIssuedReport')
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->post('/cardsissued-report', CardsIssuedReportController::class . ':CardsIssuedReport')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
 
 // ==================================================
 
 // ================== Stock =============
 
-$app -> post('/stock' , StockController::class . ':StockFilter')
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->post('/stock', StockController::class . ':StockFilter')
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
 // ==================================================
 
 // ================== Users =============
 
-$app -> post('/users' , AdminUsersController::class . ':createUsers')
--> add(new adminConference()) 
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->post('/users', AdminUsersController::class . ':createUsers')
+    ->add(new adminConference())
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
-$app -> put('/users' , AdminUsersController::class . ':editUsers')
--> add(new adminConference()) 
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->put('/users', AdminUsersController::class . ':editUsers')
+    ->add(new adminConference())
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
-$app -> delete('/users/{id}' , AdminUsersController::class . ':deleteUsers')
--> add(new adminConference()) 
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->delete('/users/{id}', AdminUsersController::class . ':deleteUsers')
+    ->add(new adminConference())
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
-$app -> post('/confirmEmail' , AdminUsersController::class . ':emailVerification')
--> add(new adminConference()) 
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->post('/confirmEmail', AdminUsersController::class . ':emailVerification')
+    ->add(new adminConference())
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
-$app -> post('/searchUser' , AdminUsersController::class . ':UserSearchEmail') 
--> add(new adminConference()) 
--> add(new jwtDateTime())
--> add(jwtAuth());
+$app->post('/searchUser', AdminUsersController::class . ':UserSearchEmail')
+    ->add(new adminConference())
+    ->add(new jwtDateTime())
+    ->add(jwtAuth());
 
 // ==================================================
 
 
-$app -> run();
+$app->run();
